9d06860331e72badc43b389f26c810a4804dd5aa
Shubham Dutta <shubhamdutta26@gmail.com>
2026-01-14 19:43:23 -0500

    removed neorg

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -2,23 +2,6 @@
 
 return {
   {
-    "nvim-neorg/neorg",
-    enabled = false,
-    config = function()
-      require("neorg").setup({})
-    end,
-  },
-
-  {
-    "jakewvincent/mkdnflow.nvim",
-    enabled = false,
-    config = function()
-      local mkdnflow = require("mkdnflow")
-      mkdnflow.setup({})
-    end,
-  },
-
-  {
     "obsidian-nvim/obsidian.nvim",
     version = "*",
     enabled = true,
